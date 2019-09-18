7df8688de1796d233b9ae302833cc2a2814c1606
Drew Borneman <awb79@case.edu>
2019-09-18 17:16:45 -0400

    all finished except launch files

--- src/stdr_walls_code.cpp
+++ src/stdr_walls_code.cpp
@@ -1,24 +1,77 @@
 #include "ros/ros.h"
+#include <unistd.h>
+#include <stdlib.h>
 #include "std_msgs/String.h"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Vector3.h"
 #include <sstream>
+#include <string>
 
-void lidarInput(const std_msgs::String::ConstPtr& msg)
+float prevMin = 0;
+geometry_msgs::Twist des_vel;
+geometry_msgs::Twist cmd_vel;
+ros::Publisher vel_pub;
+
+void lidarInput(const sensor_msgs::LaserScan::ConstPtr& msg)
 {
-	ROS_INFO("I heard: [%s]", msg->data.c_str());
+	int rangesSize = msg->ranges.size();
+	float minRange = msg->ranges[0];
+	for(int i=1;i<rangesSize;i++){
+		if(msg->ranges[i] < minRange){
+			minRange = msg->ranges[i];		
+		};
+	};
+	//ROS_INFO("Min Range: [%f]",minRange);
+	cmd_vel = des_vel;
+	if(minRange < prevMin && minRange < 0.4f){
+		cmd_vel.linear.x = -cmd_vel.linear.x;
+		cmd_vel.linear.y = -cmd_vel.linear.y;
+		cmd_vel.linear.z = -cmd_vel.linear.z;
+		ROS_WARN("within 0.4m of a wall - reversing direction"); 	
+	};	
+	ROS_INFO("Publishing: [%f]", cmd_vel.linear.x);
+	vel_pub.publish(cmd_vel);
+	prevMin = minRange;
+}
+
+void desVelInput(const geometry_msgs::Twist::ConstPtr& msg)
+{
+	des_vel.linear = msg->linear;
+	des_vel.angular = msg->angular;
 }
 
 int main(int argc, char ** argv)
 {
+	std::string topicName;
+	std::string nmspcName;
+	int opt;
+	while ((opt = getopt(argc, (argv), "n:")) != -1) {
+		switch (opt) {
+    			case 't':
+      				topicName = strdup(optarg);
+      				break;
+    			case 'n':
+      				nmspcName = strdup(optarg);
+      				break;
+    			default:
+      				printf("The -%c is not a recognized parameter\n", opt);
+      				break;
+		
+		}
+	} 
 
-	ros::init(argc,argv,"des_vel");
+	printf("topic name: %s\n",topicName.c_str());
+
+	ros::init(argc,argv,topicName);
 
 	ros::NodeHandle n;
 
-	ros::Publisher vel_pub = n.advertise<std_msgs::String>("cmd_vel",1000);
-	ros::Publisher des_vel_pub = n.advertise<std_msgs::String>("des_vel",1000);
-	ros::Rate loop_rate(1);
+	vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel",1000);
+	ros::Subscriber des_vel_pub = n.subscribe<geometry_msgs::Twist>(topicName,1000,desVelInput);
+	//ros::Rate loop_rate(10);
 
-	ros::Subscriber sub = n.subscribe("robot0/laser_1", 1000, lidarInput);
+	ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("laser_1", 1000, lidarInput);
 
 	while (ros::ok())
 	{
@@ -38,7 +91,7 @@ int main(int argc, char ** argv)
 		chatter_pub.publish(msg);
 		*/
 		ros::spin();
-		loop_rate.sleep();
+		//loop_rate.sleep();
 	}
 
 	return 0;
